71ee1d33c6d22b021ac8c7b230ae90b54b16cde5
Bednova Ekaterina <96745291+BednovaK@users.noreply.github.com>
2022-02-07 13:55:13 +0300

    initial commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,17 +2,9 @@ public class CreditPaymentService {
     public double calculate(double termCreditMonths) {
         double summaCredit = 1_000_000;
         double procent = 9.99;
-        double payment = 0;
 
-        if (termCreditMonths == 12) {
-            payment = ((summaCredit * procent / 100 + summaCredit) / termCreditMonths - 3747);
-        }
-        if (termCreditMonths == 24) {
-            payment = ((summaCredit * procent / 100 + summaCredit) / termCreditMonths + 311);
-        }
-        if (termCreditMonths == 36) {
-            payment = ((summaCredit * procent / 100 + summaCredit) / termCreditMonths + 1709);
-        }
+        double payment = summaCredit * (procent / 100 / 12 + (procent / 100 / 12 / (Math.pow((1 + procent / 100 / 12), termCreditMonths) - 1)));
+
         return payment;
     }
 }
